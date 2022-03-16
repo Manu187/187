b312f230df32a66207d2445105e184c8932d4dea
Manu187 <101705498+Manu187@users.noreply.github.com>
2022-03-16 10:19:32 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,131 @@
+let zufallszahl = 0
+let würfel = 0
+let treffwer = 0
+input.onButtonPressed(Button.A, function () {
+    zufallszahl = randint(0, 3)
+    würfel = randint(0, 3)
+    if (zufallszahl == 3) {
+        treffwer += 1
+        basic.showLeds(`
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (würfel == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    }
+    if (würfel == 2) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            . . . . .
+            . . . # #
+            . . . # #
+            `)
+    }
+    if (würfel == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    }
+    if (würfel == 4) {
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            . . . . .
+            # # . # #
+            # # . # #
+            `)
+    }
+    if (treffwer == 2) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.clearScreen()
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        led.stopAnimation()
+        treffwer = 0
+    }
+})
+basic.forever(function () {
+	
+})
